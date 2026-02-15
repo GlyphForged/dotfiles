36c3aa4e3b1ac9d5a5b94befbce84ed2b7e35ed3
Aaron Chavez <aaron@glyphforged.com>
2026-02-14 19:10:28 -0500

    Add bashrc and gitconfig

--- updateDotfiles.sh
+++ updateDotfiles.sh
@@ -1,14 +1,36 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+HOME_FILES=(
+  ".bashrc"
+  ".gitconfig"
+)
 
-DEST=$HOME/.config
-mkdir -p "$DEST"
+CONFIG_DIRS=(
+  "nvim"
+  "hypr"
+  "waybar"
+  "wezterm"
+)
+
+mkdir -p "$HOME/.config"
 
 echo "Updating Dotfiles..."
-for dir in */; do
-  [ -d "$dir" ] || continue
-  echo "Copying $dir -> $DEST"
-  cp -a "$dir" "$DEST/"
+
+# --- Copy HOME files ---
+for file in "${HOME_FILES[@]}"; do
+  if [[ -e "$file" ]]; then
+    echo "Syncing $file -> $HOME/$file"
+    rsync -a "$file" "$HOME/$file"
+  fi
 done
-echo "Dotfiles have been updated."
+
+# --- Copy .config dirs ---
+for dir in "${CONFIG_DIRS[@]}"; do
+  if [[ -d "$dir" ]]; then
+    echo "Syncing $dir -> $HOME/.config/"
+    rsync -a --delete "$dir/" "$HOME/.config/$dir/"
+  fi
+done
+
+echo "Done."
